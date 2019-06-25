1421be0ac58fa20067ca56a726b733250548f0b8
Billy Larru <larru91@gmail.com>
2019-06-24 19:13:38 -0500

    generacion de contenido dinamico

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin')
 const express = require('express')
 const engines = require('consolidate')
 const app = express()
+
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+)
+
+
+async function getFacts(){
+  const snapshots = await firebaseApp.firestore().collection('facts').get()
+  const facts = snapshots.docs.map((snapshot) => snapshot.data())
+  return facts
+}
 
 app.engine('hbs', engines.handlebars)
 app.set('views', './views')
 app.set('view engine', 'hbs')
 
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`)
-})
 
-app.get('/timestamp-cached', (request, response) => {
+app.get('/', (request, response) => {
   response.set('Cache-Control', 'public, max-age=300, s-maxage=600')
-  response.send(`${Date.now()}`)
+  getFacts().then((facts) => {
+    response.render('index', { facts })
+    return undefined
+  })
+  .catch((error) => {
+    response.send('error')
+  })
 })
 
 
